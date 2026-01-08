c490b28510dcbc66517d2d3acda0dd005f3e44f8
giwonn <l4279625@gmail.com>
2026-01-09 01:19:25 +0900

    chore: jpa 세팅 변경

---
+++ modules/jpa/src/main/java/com/example/jpa/config/JpaConfig.java
@@ -0,0 +1,13 @@
+package com.example.jpa.config;
+
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EntityScan("com.example")
+@EnableJpaRepositories(basePackages = "com.example.services")
+public class JpaConfig {
+}
--- services/build.gradle.kts
+++ services/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    `java-library`
+}
+
 dependencies {
-    implementation(project(":modules:jpa"))
+    api(project(":modules:jpa"))
 }
