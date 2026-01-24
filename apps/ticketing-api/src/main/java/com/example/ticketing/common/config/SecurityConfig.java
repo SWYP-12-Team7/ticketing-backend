da679372a7a1373f083de6e82a4546d0c7e43ba1
Jesuji <jesuji030502@naver.com>
2026-01-24 17:26:30 +0900

    refactor: JwtAuthenticationFilter 로그인 브랜치로 이동

--- apps/ticketing-api/src/main/java/com/example/ticketing/common/config/SecurityConfig.java
+++ apps/ticketing-api/src/main/java/com/example/ticketing/common/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.ticketing.common.config;
 
-import com.example.ticketing.common.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
