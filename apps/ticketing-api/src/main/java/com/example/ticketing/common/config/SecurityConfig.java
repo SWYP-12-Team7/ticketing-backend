c10b12eb29d3c838f7f4babbdd421a9414ca5299
Jesuji <jesuji030502@naver.com>
2026-02-05 21:19:07 +0900

    MainPage패키지, popup 관련 파일 복구

--- apps/ticketing-api/src/main/java/com/example/ticketing/common/config/SecurityConfig.java
+++ apps/ticketing-api/src/main/java/com/example/ticketing/common/config/SecurityConfig.java
@@ -39,6 +39,8 @@ public class SecurityConfig {
           // Popup API
           "/api/popups/**",
           "/popups/**",
+          // Main Page API
+          "/main/**",
           // Exhibition API
           "/api/exhibitions/**"
   };
